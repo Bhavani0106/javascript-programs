138ea2bc12e32e179139849f1cd6c22df45cdf0b
Bhavani0106 <yarrambhavani6@gmail.com>
2023-12-01 15:39:26 +0530

    main

---
+++ dis2.js
@@ -0,0 +1,32 @@
+let employees=[{id:101,name:"Rahul",sal:45000},
+               {id:102,name:"Sonia",sal:55000},
+               {id:103,name:"Priyanka",sal:65000}]
+
+
+/* for(var i=0;i<=2;i++){
+    console.log(employees[i].sal)
+}  */
+
+
+
+//print employee names
+
+console.log(employees[0])
+console.log(employees[1])
+console.log(employees[2])
+
+
+console.log(employees[0].id)
+console.log(employees[1].id)
+console.log(employees[2].id) 
+
+console.log(employees[0].name)
+console.log(employees[1].name)
+console.log(employees[2].name)
+
+console.log(employees[0].sal)
+console.log(employees[1].sal)
+console.log(employees[2].sal)
+
+
+
